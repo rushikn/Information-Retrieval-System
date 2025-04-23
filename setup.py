69467729bf4b392f1d333d1a5713ace4ee721d3e
rushikn <rushi.kaja14@gmail.com>
2025-04-23 23:01:38 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Rushi',
+    author_email='rushi@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
